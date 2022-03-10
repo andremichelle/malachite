e9a7713ab2537b482b477fc6ab5701cdf3d62d4b
andremichelle <andre.michelle@gmail.com>
2022-03-10 18:58:34 +0100

    refresh bin/

--- bin/lib/colors.d.ts
+++
@@ -1,7 +0,0 @@
-import { Random } from "./math.js";
-export declare class Colors {
-    static hslToRgb(h?: number, s?: number, l?: number): number;
-    static getRandomPalette(random: Random): number[];
-    static getPaletteByIndex(index: number): number[];
-    private static PALETTES;
-}
--- bin/meter/message.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type UpdateMeterMessage = {
-    type: "update-meter";
-    maxSquares: Float32Array[];
-    maxPeaks: Float32Array[];
-};
